e35f334dbfdd69629728cc7ee5924ca72a343afd
Alexander Ponomarev <taump@yandex.ru>
2024-06-28 17:02:59 +0400

    add eu (#2)

--- soccerCompetitions.js
+++ soccerCompetitions.js
@@ -1,1 +1,3 @@
-exports.soccerCompetitions = [2000, 2001, 2002, 2003, 2013, 2014, 2015, 2016, 2017, 2019, 2021, 2024, 2029, 2044, 2152];
+exports.soccerCompetitions = [2000, 2001, 2002, 2003, 2013, 2014, 2015, 2016, 2017, 2019, 2021, 2024, 2029, 2018, 2152];
+
+// UPDATE: remove 2044 (Chinese Super League) and Add 2018 (European Championship)
